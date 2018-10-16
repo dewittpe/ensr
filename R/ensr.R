534e00cf92227a924c2a0ad5cc342df132ebb3ea
Peter DeWitt <peter.dewitt@ucdenver.edu>
2018-10-16 11:05:07 -0600

    predict method cleaned up

--- R/ensr.R
+++ R/ensr.R
@@ -7,12 +7,12 @@
 #'
 #' @export
 ensr <- function(x, y, alphas = seq(0.05, 0.95, by = 0.05), nfolds = 10L, foldid, ...) {
-  
+
   # build a single set of folds
   if (missing(foldid)) {
     foldid <- rep(seq(nfolds), length.out = nrow(x))
   }
-  
+
   cl <- as.list(match.call())
   cl[[1]] <- quote(glmnet::cv.glmnet)
 
@@ -34,7 +34,7 @@ summary.ensr <- function(object, ...) {
            function(m) {
              c(lmin = which(sapply(m$lambda, function(l) isTRUE(all.equal(target = m$lambda.min, l)))),
                l1se = which(sapply(m$lambda, function(l) isTRUE(all.equal(target = m$lambda.1se, l)))))
-           }) 
+           })
 
   data.table::rbindlist(
                         Map(function(model, index) {
@@ -54,23 +54,83 @@ summary.ensr <- function(object, ...) {
   )
 }
 
-#' @inheritParams glmnet::coef.cv.glmnet
+#' Predict Methods for ensr objects
+#'
+#' Using either the \code{lambda.min} or \code{lambda.1se}, find the preferable
+#' model from the \code{ensr} object and return a prediction.
+#'
+#' The \code{glmnet::predict} argument \code{s} is ignored if specified and
+#' attempted to be passed via \code{...}.  The value of \code{s} that is passed
+#' to \code{glmnet::predict} is determined by the value of \code{lambda.min} or
+#' \code{lambda.1se} found from a call to \code{\link{preferable}}.
+#'
+#' @inheritParams glmnet::predict.elnet
+#' @param object a \code{ensr} object
+#' @param cve a character string with the value "min" or "1se".  The default is "min".
+#' @param ... other arguments passed along to \code{predict}
+#' @name predict
 #' @export
-coef.ensr <- function(object, model_idx, s = "min", exact = FALSE, ...) {
-  if (missing(model_idx) && is.character(s)) { 
-    sm <- summary(object)
-    sm <- sm[which.min(sm[[paste0('cve.', s)]]), ]
-    model_idx <- sm$model_idx
-    s <- sm[[paste0("lambda.", s)]]
-  } 
-  stats::coef(object[[model_idx]], s = s, exact = FALSE, ...)
+predict.ensr <- function(object, newx, cve = NULL, type=c("link","response","coefficients","nonzero","class"), exact = FALSE, newoffset, ...) {
+
+  cl <- as.list(match.call())
+  cl[[1]] <- quote(predict)
+
+  if (is.null(cve)) {
+    cve <- "min"
+  }
+
+  if (!(cve %in% c("min", "1se"))) {
+    warning(sprintf("%s is not valid for cve in predict.ensr, setting to 'min'.", cve))
+    cve <- "min"
+  }
+
+  po <- preferable(object, cve)
+  cl$object <- po
+
+  cl$s <- po[[paste0("lambda.", cve)]]
+  eval(as.call(cl))
 }
 
-# Extract Model Coefficients
-#
-# @param model_idx the index of the ensr object referring to the cv.glmnet
-# object to extract coeffiencts from.  This another way of getting at the alpha
-# value.
-# @param s either a character string of "min" or "1se" to indicate using the
-# lambda value associated with the minimum cross-validation error or the 1se
+#' @rdname predict
+#' @export
+coef.ensr <- function(object, cve = NULL, exact = FALSE, ...) {
+  cl <- as.list(match.call())
+  cl[[1]] <- quote(predict)
+  cl$type = "coefficients"
+  eval(as.call(cl))
+}
 
+#' Preferable Elastic Net Model
+#'
+#' Find the preferable Elastic Net Model from an ensr object.  The preferable
+#' model is defined as the model with the lowest mean cross validation error and
+#' largest alpha value.
+#'
+#' @param object an ensr object
+#' @param cve a character string, by default using \code{"min"} will use the
+#' \code{lambda.min} for selecting a preferable model.  \code{"1se"} will use
+#' \code{lambda.1se}.
+#' @param ... not currently used.
+#' @export
+preferable <- function(object, cve = "min", ...) {
+  UseMethod("preferable")
+}
+
+#' @export
+preferable.ensr <- function(object, cve = "min", ...) {
+
+  ce <- paste0("cve.", cve)
+  le <- paste0("lambda.", cve)
+
+  sm <- summary(object)
+  sm <- sm[sm[[ce]] == min(sm[[ce]]), ]
+  if (nrow(sm) > 1L) {
+    sm <- sm[sm[['alpha']] == max(sm[['alpha']])]
+  }
+  model_idx <- sm$model_idx
+  # s <- sm[[le]]
+
+  out <- object[[model_idx]]
+  out$alpha <- sm$alpha
+  out
+}
