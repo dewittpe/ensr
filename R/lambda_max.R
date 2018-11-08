326641db1a26559e12a07f5fd3eb8ae9d0c64dd2
Peter DeWitt <peter.dewitt@ucdenver.edu>
2018-11-08 14:24:25 -0700

    wip: scratch work for lambda_alpha_grid

--- R/lambda_max.R
+++ R/lambda_max.R
@@ -74,3 +74,61 @@ lambda_max <- function(y, x, standardize = TRUE, standardize.response = FALSE, a
   max(abs(t(x) %*% (residuals(lmfit) + predict(lmfit, type = "terms")))) / (alpha * nrow(x))
   
 }
+
+#' Lambda Alpha Grid
+#'
+#' Construct a data frame with values for lambda and alpha with an indicator to
+#' know if the model is worth fitting.
+#'
+#' @export
+lambda_alpha_grid <- function(lambdas, alphas) {
+
+}
+alphas <- seq(0, 1, length = 10)
+lambdas <- lambda_max(Yvec, Xmat, alpha = alphas)
+
+all_alphas <- sort(c(alphas, alphas[-length(alphas)] + diff(alphas) / 2))
+all_lmaxs  <- rep(lambdas, each = 2)
+
+top <- data.frame(l = all_lmaxs[-length(all_lmaxs)], a = all_alphas)
+library(data.table)
+top <- as.data.table(top)
+
+lgrids <-
+  Map(function(a, lmax, lmin) { data.table(a = a, l = 10^seq(log10(lmin), log10(lmax), length = 50)) },
+      a = top$a,
+      lmax = top$l,
+      MoreArgs = list(lmin = 0.001 * min(all_lmaxs)))
+
+alll <- unique(unlist(lapply(lgrids, `[[`, "l")))
+
+lgrid <-
+  Map(function(a, l) { data.table(a = a, l = alll[which(alll <= l)]) }, a = top$a, l = top$l)
+lgrid <- rbindlist(lgrid)
+
+
+top
+
+
+summary(lgrid)
+
+with(top, { plot(a, log10(l), type = "l") })
+with(lgrid, points(a, log10(l), pch = "."))
+
+lgrid$z <- cos(lgrid$a) + sin(log10(lgrid$l))
+
+which.min(lgrid$z)
+
+
+library(ggplot2)
+
+ggplot(lgrid) +
+  theme_bw() +
+  aes(x = a, y = log10(l), z = z, color = z, fill = z) +
+  geom_contour() +
+  geom_point(data = lgrid[which.min(lgrid$z)], color = "red")
+
+
+
+
+
