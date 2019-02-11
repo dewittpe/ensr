f69a98ff7129356e5f310d37ab210c39fa6095ad
Peter DeWitt <peter.dewitt@ucdenver.edu>
2019-02-10 23:15:42 -0700

    format code and added commented errors and warnings for issue-0006.R
    re #6

--- examples/github/issue-0006.R
+++ examples/github/issue-0006.R
@@ -3,26 +3,39 @@ library(ensr)
 
 set.seed(2785)
 
-y = sample(c('Y','N'), 100, replace = T)
-yb = as.numeric(y=='Y')
+y  <- sample(c('Y','N'), 100, replace = TRUE)
+yb <- as.numeric(y=='Y')
 
-x1 = sample(c(0,1), 100, replace = T)
-x2 = sample(c(0,1), 100, replace = T)
-x3 = rep(0, 100)
+x1 <- sample(c(0,1), 100, replace = TRUE)
+x2 <- sample(c(0,1), 100, replace = TRUE)
+x3 <- rep(0, 100)
+
 mydata <- data.frame(y = as.factor(y), yb, x1, x2, x3 )
-head(mydata)
+str(mydata)
 
-
-x <- model.matrix(y ~ x1 + x2 , data = mydata)[,-1]
-xb <- model.matrix(y ~ x1 + x2 + x3, data = mydata)[,-1]
+x  <- model.matrix(y ~ x1 + x2 - 1,      data = mydata)
+xb <- model.matrix(y ~ x1 + x2 + x3 - 1, data = mydata)
 
 
 ### 1: glmnet runs with 2-level factor for family = "binomial, ensr does not
-glmmod <- glmnet(x , y=mydata$y, alpha=1, family = "binomial")
+glmmod <- glmnet(x = x, y = mydata$y, alpha = 1, family = "binomial")
 
-ensr_obj <- ensr(x, y=mydata$y, family = "binomial")  
+ensr_obj <- ensr(x = x, y = mydata$y,  family = "binomial")
+# Error in seq.default(log10(lmin), log10(lmax), length = nlambda) :
+#   'from' must be a finite number
+# In addition: Warning messages:
+# 1: In model.response(mf, "numeric") :
+#   using type = "numeric" with a factor response will be ignored
+# 2: In Ops.factor(y, z$residuals) : ‘-’ not meaningful for factors
+# 3: In Ops.factor(stats::residuals(lmfit), stats::predict(lmfit, type = "terms")) :
+#   ‘+’ not meaningful for factors
 
-ensr_obj <- ensr(x, mydata$yb, family = "binomial")  
+ensr_obj <- ensr(x = x, y = mydata$yb, family = "binomial")
+# There were 50 or more warnings (use warnings() to see the first 50)
+
+summary(warnings())
+# 50 identical warnings:
+# In regularize.values(x, y, ties, missing(ties)) : collapsing to unique 'x' values
 
 
 
@@ -30,7 +43,9 @@ ensr_obj <- ensr(x, mydata$yb, family = "binomial")
 
 glmmod <- glmnet(xb , y=mydata$yb, alpha=1, family = "binomial")
 
-ensr_obj <- ensr(xb, y=mydata$yb, family = "binomial")  
+ensr_obj <- ensr(xb, y=mydata$yb, family = "binomial")
+# Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) :
+#   0 (non-NA) cases
 
 
 
